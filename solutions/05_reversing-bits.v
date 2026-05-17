d658be58499edf0d0f91578efe5b8a06729e78e3
lin02318 <tal96@cornell.edu>
2026-05-17 15:27:35 -0400

    Add 05_reversing-bits

---
+++ solutions/05_reversing-bits.v
@@ -0,0 +1,16 @@
+module model #(parameter
+  DATA_WIDTH=32
+) (
+  input  [DATA_WIDTH-1:0] din,
+  output logic [DATA_WIDTH-1:0] dout
+);
+
+  always_comb begin
+
+    for ( int i = 0; i < DATA_WIDTH; i++ ) begin
+      dout[i] = din[DATA_WIDTH-i-1];
+    end
+
+  end  // always_comb
+
+endmodule
